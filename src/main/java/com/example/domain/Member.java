6ef65b198d93b6afa848b1cfca86d14d373bce3b
ishibashikaisei <kaisei.ishibashi@rakus-partners.co.jp>
2022-05-10 15:14:38 +0900

    getter/setter/toStringの追加

--- src/main/java/com/example/domain/Member.java
+++ src/main/java/com/example/domain/Member.java
@@ -1,5 +1,19 @@
 package com.example.domain;
 
 public class Member {
+	String name;
+
+	@Override
+	public String toString() {
+		return "Member [name=" + name + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
