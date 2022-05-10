83cba26e912a9e4ede21708e11d56e5716152a9d
ishibashikaisei <kaisei.ishibashi@rakus-partners.co.jp>
2022-05-10 13:42:21 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleIshibashiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleIshibashiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleIshibashiApplication.class, args);
+	}
+
+}
